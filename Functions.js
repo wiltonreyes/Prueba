30fc8d17fd3729b8487e3e349d0d502acc5fd87c
Wilton <91771609+wiltonreyes@users.noreply.github.com>
2021-10-02 00:49:18 -0500

    Subir archivos

---
+++ Functions.js
@@ -0,0 +1,7 @@
+function mostrarMensaje1(){
+alert('Bienvenido al curso JavaScript de aprenderaprogramar.com');
+}
+
+function mostrarMensaje2(){
+alert('Ha hecho click sobre el párrafo inferior');
+}
